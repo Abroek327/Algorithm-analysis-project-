d6f89bde68450f29a536871253b0312a5ad5c05f
jamestranformal@gmail.com <jamestranformal@gmail.com>
2023-03-31 10:54:23 -0700

    Searching Games

--- Project-Parlay/db_operations.py
+++ Project-Parlay/db_operations.py
@@ -7,6 +7,28 @@ class db_operations():
         self.connection = sqlite3.connect(conn_path)
         self.cursor = self.connection.cursor()
         print("connection made...")
+    
+    #creates the songs table
+    def create_songs_table(self):
+        query = '''
+        CREATE TABLE songs(
+            songID VARCHAR(22) NOT NULL PRIMARY KEY,
+            Name VARCHAR(20),
+            Artist VARCHAR(20),
+            Album VARCHAR(20),
+            releaseDate DATETIME,
+            Genre VARCHAR(20),
+            Explicit BOOLEAN,
+            Duration DOUBLE,
+            Energy DOUBLE,
+            Danceability DOUBLE,
+            Acousticness DOUBLE,
+            Liveness DOUBLE,
+            Loudness DOUBLE
+        );
+        '''
+        self.cursor.execute(query)
+        print("Table Created")
 
     #retrieves a single value from a table
     def single_record(self, query):
@@ -27,24 +49,30 @@ class db_operations():
         results.remove(None)
         return results
     
+    #returns the results of a query with first column
     def name_placeholder_query(self, query, dictionary):
         self.cursor.execute(query, dictionary)
         results = self.cursor.fetchall()
         results = [i[0] for i in results]
         return results
     
-    def single_tuple(self, query):
-        self.cursor.execute(query)
-        return self.cursor.description()[0]
+    #returns results of a query for all columns
+    def name_placeholder_query2(self, query, dictionary):
+        self.cursor.execute(query, dictionary)
+        results = self.cursor.fetchall()
+        return results
     
-    def delete_record(self, query):
-        self.cursor.execute(query)
-        self.connection.commit()
-        return "Song deleted"
-    
-    def update_query(self, query, dictionary):
+    #deletes a record based on query parameter and returns confirmation of deletion
+    def delete_record(self, query, dictionary):
         self.cursor.execute(query, dictionary)
         self.connection.commit()
+        return "Song deleted.\n"
+    
+    #updates a single record's attribute based on query parameter and returns confirmation of attribute updated
+    def update_query(self, query, dictionary, attribute):
+        self.cursor.execute(query, dictionary)
+        self.connection.commit()
+        return "" + attribute + " updated.\n"
 
     
     #destructor that closes connection to database
--- Project-Parlay/parlay.py
+++ Project-Parlay/parlay.py
@@ -15,13 +15,48 @@ def is_empty():
     result = db_ops.single_record(query)
     return result == 0
     
+def search_by_artist():
+    #get a list of all artists
+    query = '''
+    SELECT DISTINCT game_id, team_home, team_away, winning_team
+    FROM NFLHistory;
+    '''
 
+    print("Games in playlist: ")
+    games = db_ops.single_attribute(query)
+    
+    #show all artists an create a dictionary of choices
+    choices = {}
+    for i in range(len(games)):
+        print(i, games[i])
+        choices[i] = games[i]
+    index = helper.get_choice(choices.keys())
+
+    print("How many songs do you want returned for "+choices[index]+"?")
+    print("Enter 1, 5 or 0 for all songs")
+    num = helper.get_choice([1,5,0])
+
+    #print results
+    query = '''
+    SELECT DISTINCT name
+    FROM songs
+    WHERE Artist =:artist
+    ORDER BY RANDOM()
+    '''
+
+    #run query for songs and print results
+    dictionary = {"artist":choices[index]}
+    if num != 0:
+        query += "LIMIT:lim"
+        dictionary["lim"] = num
+    results = db_ops.name_placeholder_query(query, dictionary)
+    helper.pretty_print(results)
 
 
     
 #Main Program
 startScreen()
-
+search_by_artist()
 
 
 #deconstruct at end
